dbcfe37c3712de5bd13ac01e7d43d6352eb1910b
idrowzen-01 <156851205+idrowzen-01@users.noreply.github.com>
2025-09-17 12:28:07 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 7; index++) {
+        basic.showString(" CALDON")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 7; index++) {
+        basic.showString(" LIZ ")
+    }
+})
